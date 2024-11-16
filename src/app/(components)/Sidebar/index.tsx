2798b1709b06b5a178874f252d6defda13989eba
andrii petryk <andrii.petryk.dev@gmail.com>
2024-11-16 16:05:36 -0500

    Add dark mode toggle and sidebar enhancements (#6)
    * Remove all project files
    
    This commit deletes all files related to the project, including configuration files, fonts, SVG assets, CSS styles, and package-related files. This action effectively clears the project's repository.
    
    * Add DashboardWrapper with Navbar and Sidebar
    
    Implemented the DashboardWrapper component to encapsulate the main layout structure, including Navbar and Sidebar components. Integrated DashboardWrapper into layout.tsx to provide a consistent UI framework across the application.
    
    * Add images and enhance Sidebar
    
    Included multiple new image files in the public directory. Updated the Sidebar component to display a logo and project name, along with a lock icon indicating a private status.
    
    * added redux toolkit
    
    * added changes to the redux.tsx
    
    * Add dark mode toggle and sidebar collapse functionality
    
    Integrated a dark mode toggle in the Navbar and updated the Sidebar to handle collapsible state. Enhanced the DashboardWrapper to manage dark mode and sidebar states using Redux selectors and useEffect.
    
    ---------
    
    Co-authored-by: Andrii Petryk <apetryk.work@gmail.com>

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -2,7 +2,10 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import { LockIcon } from "lucide-react";
+import { Home, LockIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import Link from "next/link";
 
 const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
@@ -28,10 +31,49 @@ const Sidebar = () => {
           <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
           <p className="text-xs font-medium tracking-wide text-gray-500 dark:text-gray-400">Private</p>
         </div>
+
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" isCollapsed={false}/>
+        </nav>
       </div>
     </div>
 
   </div>;
 };
+interface SidebarLinkProps {
+  href: string;
+  icon: any;
+  label: string;
+  isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+  const screenWidth = window.innerWidth;
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+
+  return (
+    <Link href={href} className="w-full">
+    <div className={`
+    relative flex cursor-pointer items-center gap-3 transition-colors
+    hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-black
+    ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""}
+    `}>
+      {isActive && <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200"/>}
+      <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100"/>
+      <span className={`font-medium text-gray-800 dark:text-gray-100}`}>
+        {label}
+      </span>
+    </div>
+  </Link>)
+}
 
 export default Sidebar;
