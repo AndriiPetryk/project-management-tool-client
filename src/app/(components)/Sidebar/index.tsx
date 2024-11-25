93b9bb7455f1905d0752621c8c824a1b2b55e16d
andrii petryk <andrii.petryk.dev@gmail.com>
2024-11-25 13:07:06 -0500

    Change sidebar (#13)
    * Remove all project files
    
    This commit deletes all files related to the project, including configuration files, fonts, SVG assets, CSS styles, and package-related files. This action effectively clears the project's repository.
    
    * Add DashboardWrapper with Navbar and Sidebar
    
    Implemented the DashboardWrapper component to encapsulate the main layout structure, including Navbar and Sidebar components. Integrated DashboardWrapper into layout.tsx to provide a consistent UI framework across the application.
    
    * Add images and enhance Sidebar
    
    Included multiple new image files in the public directory. Updated the Sidebar component to display a logo and project name, along with a lock icon indicating a private status.
    
    * added redux toolkit
    
    * added changes to the redux.tsx
    
    * Add dark mode toggle and sidebar collapse functionality
    
    Integrated a dark mode toggle in the Navbar and updated the Sidebar to handle collapsible state. Enhanced the DashboardWrapper to manage dark mode and sidebar states using Redux selectors and useEffect.
    
    * Add dark mode toggle and sidebar collapse functionality
    
    Integrated a dark mode toggle in the Navbar and updated the Sidebar to handle collapsible state. Enhanced the DashboardWrapper to manage dark mode and sidebar states using Redux selectors and useEffect.
    
    * Add imports for navigation and redux in Sidebar
    
    Integrated additional dependencies in the Sidebar component, including navigation and Redux hooks. This update enables better state management and routing functionalities.
    
    * Add collapsible functionality to sidebar
    
    Implemented state management for collapsing the sidebar. Added a new button to toggle the collapse state and modified styles accordingly to hide or show the sidebar. Updated imports and included necessary redux actions.
    
    * Add newline at end of redux.tsx file
    
    Ensure the file ends with a newline character for better compatibility and adherence to POSIX standards. This change helps maintain consistency across different text editors and operating systems.
    
    * Refactor imports and utilize useSelector in sidebar
    
    Changed import paths to use relative references within redux files for consistency. Added `useAppSelector` in `Sidebar` to manage sidebar collapse state. Minor styling adjustments in `Sidebar` for better readability.
    
    * Remove unused `isCollapsed` prop from SidebarLink
    
    The `isCollapsed` prop was removed from the `SidebarLink` component as it was not being used. This cleanup helps in reducing unnecessary code and improving maintainability.
    
    * changes
    
    * Added changes regarding sidebar
    
    ---------
    
    Co-authored-by: Andrii Petryk <apetryk.work@gmail.com>

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import { LockIcon, X } from "lucide-react";
+import { Briefcase, ChevronDown, ChevronUp, LockIcon, Search, Settings, User, Users, X } from "lucide-react";
 import Home from "@/app/page";
 import { usePathname } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
@@ -15,6 +15,7 @@ const Sidebar = () => {
 
 
   const dispatch = useAppDispatch();
+  // @ts-ignore
   const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
 
   const sideClassName = `fixed flex flex-col h-[100%] shadow-xl transition-all 
@@ -26,42 +27,54 @@ const Sidebar = () => {
         PMTL
       </div>
       {isSidebarCollapsed ? null : (
-        <button className="py-3" onClick={()=>{dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}}>
-          <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white"/>
+        <button className="py-3" onClick={() => {
+          dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+        }}>
+          <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
         </button>
       )}
     </div>
-    <div className="flex fitems-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-      <Image src="/logo.png" alt="Logo" width={40} height={40} />
-      <div className="flex flex-col">
+    <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+      <Image src="/logo.png" alt="Logo" width={40} height={40} className="w-[40px] h-[40px]" />
+      <div className="flex flex-col w-full">
         <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
-            Project Management Tool
+          Project Management Tool
         </h3>
         <div className="mt-1 flex items-start gap-2">
           <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
           <p className="text-xs font-medium tracking-wide text-gray-500 dark:text-gray-400">Private</p>
         </div>
-
-        <nav className="z-10 w-full">
-          <SidebarLink icon={Home} label="Home" href="/" isCollapsed={false}/>
-        </nav>
       </div>
     </div>
-
+    <nav className="z-10 w-full">
+      {/*<SidebarLink icon={Home} label="Home" href="/" />*/}
+      <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
+      <SidebarLink icon={Search} label="Search" href="/search" />
+      <SidebarLink icon={Settings} label="Settings" href="/settings" />
+      <SidebarLink icon={User} label="Users" href="/users" />
+      <SidebarLink icon={Users} label="Teams" href="/teams" />
+    </nav>
+    <button onClick={()=> setShowProjects( (prev) => !prev)}
+            className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+    >
+      <span className="flex items-center gap-2">Projects</span>
+      {showProjects ? (<ChevronUp className="h-5 w-5" />) : (<ChevronDown className="h-5 w-5" />)}
+    </button>
   </div>;
 };
+
 interface SidebarLinkProps {
   href: string;
   icon: any;
   label: string;
-  isCollapsed: boolean;
+  isCollapsed?: boolean;
 }
 
 const SidebarLink = ({
-  href,
-  icon: Icon,
-  label,
-}: SidebarLinkProps) => {
+                       href,
+                       icon: Icon,
+                       label
+                     }: SidebarLinkProps) => {
   const pathname = usePathname();
   const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
   const screenWidth = window.innerWidth;
@@ -71,19 +84,16 @@ const SidebarLink = ({
   const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
 
   return (
-    <Link href={href} className="w-full">
-    <div className={`
-    relative flex cursor-pointer items-center gap-3 transition-colors
-    hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-black
-    ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3
-    `}>
-      {isActive && <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200"/>}
-      <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100"/>
-      <span className={`font-medium text-gray-800 dark:text-gray-100}`}>
-        {label}
-      </span>
-    </div>
-  </Link>)
+    <Link href={href} className="w-full relative">
+      <div className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-black
+          ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3`}>
+        {isActive && <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200"/>}
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100"/>
+        <span className={`font-medium text-gray-800 dark:text-gray-100}`}>
+          {label}
+        </span>
+      </div>
+    </Link>)
 }
 
 export default Sidebar;
